f3863d694b2d07b79cb22cbde2e005414b7497b2
scratchis <103792439+scratchis@users.noreply.github.com>
2023-01-20 19:53:47 -0500

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA7S40dpUVNRjP8tUAla7IZnkhXS3BJdc8",
+    authDomain: "let-us-chat-44a97.firebaseapp.com",
+    projectId: "let-us-chat-44a97",
+    storageBucket: "let-us-chat-44a97.appspot.com",
+    messagingSenderId: "454051658818",
+    appId: "1:454051658818:web:244cb9c32bce02acc981db"
+  };
